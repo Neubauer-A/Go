04d7c1317ab6f66c585f6c660ffa76df1e1929b5
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2020-10-12 19:46:51 -0500

    Create base.py

---
+++ TMtests/tmcode/encoders/base.py
@@ -0,0 +1,27 @@
+import importlib
+
+class Encoder:
+    def name(self):  
+        raise NotImplementedError()
+
+    def encode(self, game_state): 
+        raise NotImplementedError()
+
+    def encode_point(self, point): 
+        raise NotImplementedError()
+
+    def decode_point_index(self, index): 
+        raise NotImplementedError()
+
+    def num_points(self):
+        raise NotImplementedError()
+
+    def shape(self): 
+        raise NotImplementedError()
+
+def get_encoder_by_name(name, board_size): 
+    if isinstance(board_size, int):
+        board_size = (board_size, board_size) 
+    module = importlib.import_module('tmcode.encoders.' + name)
+    constructor = getattr(module, 'create') 
+    return constructor(board_size)
