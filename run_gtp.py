51d3edde2473aa098ed370da2a4574e26aa4642e
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2020-11-01 12:34:33 -0600

    Update run_gtp.py

--- run_gtp.py
+++ run_gtp.py
@@ -1,10 +1,15 @@
 #!/usr/local/bin/python
-from gostuff.gtp import GTPFrontend
 from gostuff.agents.predict import load_prediction_agent
 from gostuff.agents import termination
+from gostuff.gtp import GTPFrontend
+from argparse import ArgumentParser
 import h5py
 
-model_file = h5py.File('/content/drive/My Drive/Colab Notebooks/Go/gostuff/models/ggb.h5', 'r')
+parser = ArgumentParser()
+parser.add_argument('--model', type=str, required=True)
+args = parser.parse_args()
+
+model_file = h5py.File(args.model, 'r')
 agent = load_prediction_agent(model_file)
 strategy = termination.get('resign')
 termination_agent = termination.TerminationAgent(agent, strategy)
