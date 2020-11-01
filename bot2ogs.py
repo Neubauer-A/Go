e4863f40eb37eee83043ede1c0584e97439c40b6
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2020-11-01 12:34:09 -0600

    Update bot2ogs.py

--- bot2ogs.py
+++ bot2ogs.py
@@ -5,16 +5,18 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('--apikey', type=str, required=True)
+parser.add_argument('--model', type=str, required=True)
 args = parser.parse_args()
 
-GTP2OGS = '/tools/node/bin/node /gtp2ogs-node/gtp2ogs/gtp2ogs.js'
-APIKEY = args.apikey
+gtp2ogs = '/tools/node/bin/node /gtp2ogs-node/gtp2ogs/gtp2ogs.js'
+apikey = args.apikey
 username = 'GoGoBoi'
+model = args.model
 
-cmd = f'{GTP2OGS} --apikey {APIKEY} --username {username} --hidden --debug --persist \
+cmd = f'{gtp2ogs} --apikey {apikey} --username {username} --hidden --debug --persist \
     --boardsizes 19 --komis all --maxconnectedgames 8 --maxhandicap 0 \
     --noautohandicap --greeting "heyyyyyyyyyy" \
     --farewell "byeeeee hope to see you again" --noclock --speeds blitz,live \
-    -- python3 run_gtp.py'
+    -- python3 run_gtp.py --{model}'
 
 os.system(cmd)
