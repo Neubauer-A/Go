61f678b3fcfc43d421681832a6ea1f83e308efd4
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2020-11-01 12:46:22 -0600

    Update run_gtp.py

--- run_gtp.py
+++ run_gtp.py
@@ -5,14 +5,18 @@ from gostuff.gtp import GTPFrontend
 from argparse import ArgumentParser
 import h5py
 
+# Loads a model and passes it to GTP command/response
+
 parser = ArgumentParser()
 parser.add_argument('--model', type=str, required=True)
 args = parser.parse_args()
 
+# Load the model from an h5 file as a prediction agent.
 model_file = h5py.File(args.model, 'r')
 agent = load_prediction_agent(model_file)
+# Wrap the prediction agent with termination strategy
 strategy = termination.get('resign')
 termination_agent = termination.TerminationAgent(agent, strategy)
-
+# Pass to GTP
 frontend = GTPFrontend(termination_agent)
 frontend.run()
