950bcaca3d9099749956cbab63c38efb731b1e12
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2020-11-01 12:36:22 -0600

    Update sgf_index.py

--- gostuff/data/sgf_index.py
+++ gostuff/data/sgf_index.py
@@ -2,7 +2,8 @@ import os
 import glob
 
 class SGFIndex:
-    def __init__(self, url='https://homepages.cwi.nl/~aeb/go/games/games.tgz'):
+    def __init__(self, size=19, url='https://homepages.cwi.nl/~aeb/go/games/games.tgz'):
+        self.size = size
         self.url = url
         self.raw_dir = [i for i in self.url[:-4]]
         while '/' in self.raw_dir:
@@ -14,7 +15,12 @@ class SGFIndex:
         os.system('wget -c %s -O - | tar -xz' %(self.url))
 
     def create_index(self):
-        nonos = ['other_sizes','unusual','misc','training']
-        for filename in glob.iglob(self.raw_dir+'/'+'**/*.sgf', recursive=True):
-            if not any(nono in filename for nono in nonos):
-                self.index.append(filename)
+        if self.size!=19:
+            for filename in glob.iglob(self.raw_dir+'/'+'**/*.sgf', recursive=True):
+                if f"{self.size}x{self.size}" in filename:
+                    self.index.append(filename)
+        else:   
+            nonos = ['other_sizes','unusual','misc','training']
+            for filename in glob.iglob(self.raw_dir+'/'+'**/*.sgf', recursive=True):
+                if not any(nono in filename for nono in nonos):
+                    self.index.append(filename)
